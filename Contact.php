e3581f5be637ae15e82b021a6fdf1a7d25844a17
Your Name <you@example.com>
2018-08-10 23:52:18 +0700

    update lần 1

---
+++ Contact.php
@@ -0,0 +1,41 @@
+<li class="agileits">
+    <!-- contact -->
+    <div class="contact agileits-w3layouts">
+        <div class="container">
+            <h3 class="agileits-title">Contact Me</h3>
+            <div class="contact-w3lsrow">
+                <div class="col-md-6 contact-wthree-left">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3023.9503398796587!2d-73.9940307!3d40.719109700000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a27e2f24131%3A0x64ffc98d24069f02!2sCANADA!5e0!3m2!1sen!2sin!4v1441710758555"
+                            allowfullscreen></iframe>
+                    <div class="faddressw3-agileinfo">
+                        <div class="faddress-w3left">
+                            <p>756 global Place, North Sydney,<br> FA-2305,Canada </p>
+                        </div>
+                        <div class="faddress-w3right">
+                            <p>Call us : +01 111 222 3333</p>
+                            <p>E-mail : <a href="mailto:info@example.com">mail@example.com</a></p>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+                <div class="col-md-6 contact-wthree-right">
+                    <form action="#" method="post">
+                        <input type="text" name="Name" placeholder="Name" required="">
+                        <input type="email" class="email" name="Email" placeholder="Email" required="">
+                        <input type="text" class="phone" name="phone" placeholder="Phone" required="">
+                        <textarea name="Message" placeholder="Message" required=""></textarea>
+                        <input type="submit" value="SUBMIT">
+                    </form>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+            <!-- footer -->
+            <div class="agile-footer">
+                <p>© 2017 Simple CV. All rights reserved | Design by <a href="http://w3layouts.com/"
+                                                                        target="_blank">w3layouts</a></p>
+            </div>
+            <!-- //footer -->
+        </div>
+    </div>
+    <!-- //contact -->
+</li>
